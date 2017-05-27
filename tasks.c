ee6420fd2a08e6030549e246c24870a40ac6d8c8
Lucas dos Santos de Souza Tramontin <hilucas42@gmail.com>
2017-05-27 10:31:54 -0300

    Simple tasks to show the semaphore and shared memory working

--- tasks.c
+++ tasks.c
@@ -10,21 +10,76 @@
 
 void task_1()
 {
+    int x;
+    int *share = (int*)shm_get(42, sizeof(int));
+    
+    short long *share_sem = (short long *)shm_get(17, sizeof(short long));
+    
+    sem_t *sem1 = createSem(1);
+    
+    share_sem[0] = (short long)sem1;
+    
+    share[0] = 0;
+    x = 1;
+    
     while(1){
-        delay(100);
+        
+        semWait(sem1);
+        
+        if(x == 0) shm_release(17);
+        
+        for(x = share[0]; x < 5; x++)
+        {
+            PORTDbits.RD0 = ~LATDbits.LD0;
+            delay_ms(500);
+        }
+        share[0] = x;
+        semPost(sem1);
     }
 }
 
 void task_2()
 {
+    int x;
+    int *share = (int**)shm_get(42, sizeof(int));
+    
+    short long *share_sem = (short long *)shm_get(17, sizeof(short long));
+    
+    sem_t *sem1 = (sem_t*)share_sem[0];
+    
     while(1){
-        // Todo code here
+        
+        semWait(sem1);
+        for(x = share[0]; x < 10; x++)
+        {
+            PORTDbits.RD1 = ~LATDbits.LD1;
+            delay_ms(500);
+        }
+        share[0] = x;
+        semPost(sem1);
     }
+    
+    exit();
 }
 
 void task_3()
 {
+    int x;
+    int *share = (int**)shm_get(42, sizeof(int));
+    
+    short long *share_sem = (short long *)shm_get(17, sizeof(short long));
+    
+    sem_t *sem1 = (sem_t*)share_sem[0];
+    
     while(1){
-        // Todo code here
+        
+        semWait(sem1);
+        for(x = share[0]; x < 15; x++)
+        {
+            PORTDbits.RD2 = ~LATDbits.LD2;
+            delay_ms(500);
+        }
+        share[0] = 0;
+        semPost(sem1);
     }
 }
